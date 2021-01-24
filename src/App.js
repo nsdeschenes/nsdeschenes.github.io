f9b0a415a40eb096cc793b071fe33448b5881c4f
nsdeschenes <nsdeschenes@outlook.com>
2021-01-24 15:08:52 -0400

    setup basic navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react' 
+import React, { Suspense } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import { Flex, Link, CSSReset } from '@chakra-ui/react'
+
+import { Navbar } from './components/Navbar'
+import { Main } from './components/Main'
 
 const App = () => {
   return (
-    <h1>Title</h1>
-  );
+    <>
+      <Flex direction="column" minHeight="100vh" bg="gray.50">
+        <CSSReset />
+        <Navbar>
+          <Link to="/">Home</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/presentations">Presentations</Link>
+        </Navbar>
+        <Main>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route exact path="/">
+                <p>Home</p>
+              </Route>
+              <Route exact path="/projects">
+                <p>Projects</p>
+              </Route>
+              <Route exact path="/presentations">
+                <p>Presentations</p>
+              </Route>
+            </Switch>
+          </Suspense>
+        </Main>
+      </Flex>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link as ReactRouterLink } from 'react-router-dom'
+import { Flex, Stack } from '@chakra-ui/react'
+import { node } from 'prop-types'
+
+export const Navbar = ({ children, ...props }) => {
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding={{ sm: '0.6rem', md: '0.80rem', lg: '1rem', xl: '1rem' }}
+      bg="#fff"
+      py="4px"
+      color="primary"
+      borderBottom="2px solid"
+      borderBottomColor="gray.300"
+      display={{ base: 'none', md: 'flex' }}
+      {...props}
+    >
+      <Flex
+        maxW={{ sm: 540, md: 768, lg: 960, xl: 1200 }}
+        mx="auto"
+        px={4}
+        w="100%"
+        align="center"
+        direction="row"
+      >
+        <Stack
+          isInline
+          alignItems="center"
+          flexWrap="wrap"
+          spacing="6"
+          w="100%"
+        >
+          {React.Children.map(children, (child) => {
+            if (child !== null) {
+              return React.cloneElement(child, {
+                as: ReactRouterLink,
+              })
+            }
+          })}
+        </Stack>
+      </Flex>
+    </Flex>
+  )
+}
+
+Navbar.propTypes = {
+  children: node,
+}
