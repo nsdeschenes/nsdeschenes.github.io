c929a0c3eb23a41d973c739ab7be62726ba78ac8
nsdeschenes <nsdeschenes@outlook.com>
2021-01-25 19:09:16 -0400

    update projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Box, SimpleGrid } from '@chakra-ui/react'
+
+import projects from '../data/projects'
+import ProjectCard from '../components/ProjectCard'
 
 const Projects = () => {
   return (
-    <p>Projects</p>
+    <Box width="fit-content" mx="auto">
+      <SimpleGrid columns="3" spacing="60px" mx="auto">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
+      </SimpleGrid>
+    </Box>
   )
 }
 
