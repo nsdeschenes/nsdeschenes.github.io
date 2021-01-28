69fa168c6795f95ee8a0ee59ba61cd170a760247
nsdeschenes <nsdeschenes@outlook.com>
2021-01-27 21:06:11 -0400

    started working on first footer item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { lazy, Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom'
-import { Flex, Link, CSSReset } from '@chakra-ui/react'
+import { Flex, Link, CSSReset, Image } from '@chakra-ui/react'
 
 import { Navbar } from './components/Navbar'
 import { Main } from './components/Main'
+import { Footer } from './components/Footer'
+import GithubLogo from './images/github.svg'
 
 const Home = lazy(() => import('./pages/Home'))
 const Projects = lazy(() => import('./pages/Projects'))
@@ -34,6 +36,14 @@ const App = () => {
             </Switch>
           </Suspense>
         </Main>
+        <Footer>
+          <Link href="https://github.com/nsdeschenes" target="_blank">
+            <Flex verticalAlign="middle">
+              <Image align="vertical" alt="Github Logo" src={GithubLogo} mr="5px" />
+              Github
+            </Flex>
+          </Link>
+        </Footer>
       </Flex>
     </>
   )
